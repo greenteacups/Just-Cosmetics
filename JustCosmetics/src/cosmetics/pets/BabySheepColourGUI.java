db90d37e032ad54bf081d8b7c340a85a2f6c5112
greenteacups <65455232+greenteacups@users.noreply.github.com>
2020-07-19 18:17:24 +1000

    Add files via upload

--- JustCosmetics/src/cosmetics/pets/BabySheepColourGUI.java
+++ JustCosmetics/src/cosmetics/pets/BabySheepColourGUI.java
@@ -20,6 +20,16 @@ public class BabySheepColourGUI implements Listener {
     public BabySheepColourGUI(Cosmetics b) {
         plugin = b;
     }
+    
+    // GuiConstructor
+    public void GuiConstructor(Player player, Material item, int pos, int price, String name, String desc) {
+        if (plugin.dataCosmetics.exists(player.getUniqueId(), name)) {
+            inv.setItem(pos, createGuiItem(item, ChatColor.GOLD + name, desc));
+        }
+        else {
+            inv.setItem(pos, createGuiItem(item, ChatColor.GOLD + name, price + " Slime"));
+        }
+    }
 
     public void ExampleGui(Player player) {
         // Create a new inventory, with no owner (as this isn't a real inventory), a size of nine, called example
@@ -32,117 +42,23 @@ public class BabySheepColourGUI implements Listener {
 
     // You can call this whenever you want to put the items in
     public void initializeItems(Player player) {
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Red Baby Sheep Pet")) {
-            inv.setItem(10, createGuiItem(Material.RED_DYE, ChatColor.GOLD + "Red Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(10, createGuiItem(Material.RED_DYE, ChatColor.GOLD + "Unlock Red Baby Sheep Pet", "100 Slime"));
-        }
+        GuiConstructor(player, Material.RED_DYE, 10, 100, "Red Baby Sheep Pet", "");
+        GuiConstructor(player, Material.ORANGE_DYE, 11, 100, "Orange Baby Sheep Pet", "");
+        GuiConstructor(player, Material.YELLOW_DYE, 12, 100, "Yellow Baby Sheep Pet", "");
+        GuiConstructor(player, Material.GREEN_DYE, 13, 100, "Green Baby Sheep Pet", "");
+        GuiConstructor(player, Material.LIME_DYE, 14, 100, "Lime Baby Sheep Pet", "");
+        GuiConstructor(player, Material.BLUE_DYE, 15, 100, "Blue Baby Sheep Pet", "");
+        GuiConstructor(player, Material.CYAN_DYE, 16, 100, "Cyan Baby Sheep Pet", "");
+        GuiConstructor(player, Material.LIGHT_BLUE_DYE, 19, 100, "Light Blue Baby Sheep Pet", "");
+        GuiConstructor(player, Material.PURPLE_DYE, 20, 100, "Purple Baby Sheep Pet", "");
+        GuiConstructor(player, Material.MAGENTA_DYE, 21, 100, "Magenta Baby Sheep Pet", "");
+        GuiConstructor(player, Material.PINK_DYE, 22, 100, "Pink Baby Sheep Pet", "");
+        GuiConstructor(player, Material.BROWN_DYE, 23, 100, "Brown Baby Sheep Pet", "");
+        GuiConstructor(player, Material.BLACK_DYE, 24, 100, "Black Baby Sheep Pet", "");
+        GuiConstructor(player, Material.GRAY_DYE, 25, 100, "Gray Baby Sheep Pet", "");
+        GuiConstructor(player, Material.LIGHT_GRAY_DYE, 28, 100, "Light Gray Baby Sheep Pet", "");
+        GuiConstructor(player, Material.WHITE_DYE, 29, 100, "White Baby Sheep Pet", "");
         
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Orange Baby Sheep Pet")) {
-            inv.setItem(11, createGuiItem(Material.ORANGE_DYE, ChatColor.GOLD + "Orange Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(11, createGuiItem(Material.ORANGE_DYE, ChatColor.GOLD + "Unlock Orange Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Yellow Baby Sheep Pet")) {
-            inv.setItem(12, createGuiItem(Material.YELLOW_DYE, ChatColor.GOLD + "Yellow Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(12, createGuiItem(Material.YELLOW_DYE, ChatColor.GOLD + "Unlock Yellow Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Green Baby Sheep Pet")) {
-            inv.setItem(13, createGuiItem(Material.GREEN_DYE, ChatColor.GOLD + "Green Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(13, createGuiItem(Material.GREEN_DYE, ChatColor.GOLD + "Unlock Green Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Lime Baby Sheep Pet")) {
-            inv.setItem(14, createGuiItem(Material.LIME_DYE, ChatColor.GOLD + "Lime Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(14, createGuiItem(Material.LIME_DYE, ChatColor.GOLD + "Unlock Lime Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Blue Baby Sheep Pet")) {
-            inv.setItem(15, createGuiItem(Material.BLUE_DYE, ChatColor.GOLD + "Blue Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(15, createGuiItem(Material.BLUE_DYE, ChatColor.GOLD + "Unlock Blue Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Cyan Baby Sheep Pet")) {
-            inv.setItem(16, createGuiItem(Material.CYAN_DYE, ChatColor.GOLD + "Cyan Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(16, createGuiItem(Material.CYAN_DYE, ChatColor.GOLD + "Unlock Cyan Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Light Blue Baby Sheep Pet")) {
-            inv.setItem(19, createGuiItem(Material.LIGHT_BLUE_DYE, ChatColor.GOLD + "Light Blue Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(19, createGuiItem(Material.LIGHT_BLUE_DYE, ChatColor.GOLD + "Unlock Light Blue Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Purple Baby Sheep Pet")) {
-            inv.setItem(20, createGuiItem(Material.PURPLE_DYE, ChatColor.GOLD + "Purple Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(20, createGuiItem(Material.PURPLE_DYE, ChatColor.GOLD + "Unlock Purple Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Magenta Baby Sheep Pet")) {
-            inv.setItem(21, createGuiItem(Material.MAGENTA_DYE, ChatColor.GOLD + "Magenta Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(21, createGuiItem(Material.MAGENTA_DYE, ChatColor.GOLD + "Unlock Magenta Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Pink Baby Sheep Pet")) {
-            inv.setItem(22, createGuiItem(Material.PINK_DYE, ChatColor.GOLD + "Pink Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(22, createGuiItem(Material.PINK_DYE, ChatColor.GOLD + "Unlock Pink Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Brown Baby Sheep Pet")) {
-            inv.setItem(23, createGuiItem(Material.BROWN_DYE, ChatColor.GOLD + "Brown Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(23, createGuiItem(Material.BROWN_DYE, ChatColor.GOLD + "Unlock Brown Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Black Baby Sheep Pet")) {
-            inv.setItem(24, createGuiItem(Material.BLACK_DYE, ChatColor.GOLD + "Black Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(24, createGuiItem(Material.BLACK_DYE, ChatColor.GOLD + "Unlock Black Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Gray Baby Sheep Pet")) {
-            inv.setItem(25, createGuiItem(Material.GRAY_DYE, ChatColor.GOLD + "Gray Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(25, createGuiItem(Material.GRAY_DYE, ChatColor.GOLD + "Unlock Gray Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Light Gray Baby Sheep Pet")) {
-            inv.setItem(28, createGuiItem(Material.LIGHT_GRAY_DYE, ChatColor.GOLD + "Light Gray Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(28, createGuiItem(Material.LIGHT_GRAY_DYE, ChatColor.GOLD + "Unlock Light Gray Baby Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "White Sheep Pet")) {
-            inv.setItem(29, createGuiItem(Material.WHITE_DYE, ChatColor.GOLD + "White Baby Sheep Pet"));
-        }
-        else {
-            inv.setItem(29, createGuiItem(Material.WHITE_DYE, ChatColor.GOLD + "Unlock White Baby Sheep Pet", "100 Slime"));
-        }
         
         inv.setItem(39, createGuiItem(Material.ARROW, "Back"));
         inv.setItem(40, createGuiItem(Material.BARRIER, "Remove Pet"));
--- JustCosmetics/src/cosmetics/pets/SheepColourGUI.java
+++ JustCosmetics/src/cosmetics/pets/SheepColourGUI.java
@@ -21,6 +21,16 @@ public class SheepColourGUI implements Listener {
         plugin = b;
     }
     
+    // GuiConstructor
+    public void GuiConstructor(Player player, Material item, int pos, int price, String name, String desc) {
+        if (plugin.dataCosmetics.exists(player.getUniqueId(), name)) {
+            inv.setItem(pos, createGuiItem(item, ChatColor.GOLD + name, desc));
+        }
+        else {
+            inv.setItem(pos, createGuiItem(item, ChatColor.GOLD + name, price + " Slime"));
+        }
+    }
+    
     public void ExampleGui(Player player) {
         // Create a new inventory, with no owner (as this isn't a real inventory), a size of nine, called example
         inv = Bukkit.createInventory(null, 54, ChatColor.GOLD + ""  
@@ -32,117 +42,22 @@ public class SheepColourGUI implements Listener {
 
     // You can call this whenever you want to put the items in
     public void initializeItems(Player player) {
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Red Sheep Pet")) {
-            inv.setItem(10, createGuiItem(Material.RED_DYE, ChatColor.GOLD + "Red Sheep Pet"));
-        }
-        else {
-            inv.setItem(10, createGuiItem(Material.RED_DYE, ChatColor.GOLD + "Unlock Red Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Orange Sheep Pet")) {
-            inv.setItem(11, createGuiItem(Material.ORANGE_DYE, ChatColor.GOLD + "Orange Sheep Pet"));
-        }
-        else {
-            inv.setItem(11, createGuiItem(Material.ORANGE_DYE, ChatColor.GOLD + "Unlock Orange Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Yellow Sheep Pet")) {
-            inv.setItem(12, createGuiItem(Material.YELLOW_DYE, ChatColor.GOLD + "Yellow Sheep Pet"));
-        }
-        else {
-            inv.setItem(12, createGuiItem(Material.YELLOW_DYE, ChatColor.GOLD + "Unlock Yellow Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Green Sheep Pet")) {
-            inv.setItem(13, createGuiItem(Material.GREEN_DYE, ChatColor.GOLD + "Green Sheep Pet"));
-        }
-        else {
-            inv.setItem(13, createGuiItem(Material.GREEN_DYE, ChatColor.GOLD + "Unlock Green Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Lime Sheep Pet")) {
-            inv.setItem(14, createGuiItem(Material.LIME_DYE, ChatColor.GOLD + "Lime Sheep Pet"));
-        }
-        else {
-            inv.setItem(14, createGuiItem(Material.LIME_DYE, ChatColor.GOLD + "Unlock Lime Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Blue Sheep Pet")) {
-            inv.setItem(15, createGuiItem(Material.BLUE_DYE, ChatColor.GOLD + "Blue Sheep Pet"));
-        }
-        else {
-            inv.setItem(15, createGuiItem(Material.BLUE_DYE, ChatColor.GOLD + "Unlock Blue Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Cyan Sheep Pet")) {
-            inv.setItem(16, createGuiItem(Material.CYAN_DYE, ChatColor.GOLD + "Cyan Sheep Pet"));
-        }
-        else {
-            inv.setItem(16, createGuiItem(Material.CYAN_DYE, ChatColor.GOLD + "Unlock Cyan Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Light Blue Sheep Pet")) {
-            inv.setItem(19, createGuiItem(Material.LIGHT_BLUE_DYE, ChatColor.GOLD + "Light Blue Sheep Pet"));
-        }
-        else {
-            inv.setItem(19, createGuiItem(Material.LIGHT_BLUE_DYE, ChatColor.GOLD + "Unlock Light Blue Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Purple Sheep Pet")) {
-            inv.setItem(20, createGuiItem(Material.PURPLE_DYE, ChatColor.GOLD + "Purple Sheep Pet"));
-        }
-        else {
-            inv.setItem(20, createGuiItem(Material.PURPLE_DYE, ChatColor.GOLD + "Unlock Purple Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Magenta Sheep Pet")) {
-            inv.setItem(21, createGuiItem(Material.MAGENTA_DYE, ChatColor.GOLD + "Magenta Sheep Pet"));
-        }
-        else {
-            inv.setItem(21, createGuiItem(Material.MAGENTA_DYE, ChatColor.GOLD + "Unlock Magenta Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Pink Sheep Pet")) {
-            inv.setItem(22, createGuiItem(Material.PINK_DYE, ChatColor.GOLD + "Pink Sheep Pet"));
-        }
-        else {
-            inv.setItem(22, createGuiItem(Material.PINK_DYE, ChatColor.GOLD + "Unlock Pink Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Brown Sheep Pet")) {
-            inv.setItem(23, createGuiItem(Material.BROWN_DYE, ChatColor.GOLD + "Brown Sheep Pet"));
-        }
-        else {
-            inv.setItem(23, createGuiItem(Material.BROWN_DYE, ChatColor.GOLD + "Unlock Brown Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Black Sheep Pet")) {
-            inv.setItem(24, createGuiItem(Material.BLACK_DYE, ChatColor.GOLD + "Black Sheep Pet"));
-        }
-        else {
-            inv.setItem(24, createGuiItem(Material.BLACK_DYE, ChatColor.GOLD + "Unlock Black Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Gray Sheep Pet")) {
-            inv.setItem(25, createGuiItem(Material.GRAY_DYE, ChatColor.GOLD + "Gray Sheep Pet"));
-        }
-        else {
-            inv.setItem(25, createGuiItem(Material.GRAY_DYE, ChatColor.GOLD + "Unlock Gray Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "Light Gray Sheep Pet")) {
-            inv.setItem(28, createGuiItem(Material.LIGHT_GRAY_DYE, ChatColor.GOLD + "Gray Sheep Pet"));
-        }
-        else {
-            inv.setItem(28, createGuiItem(Material.LIGHT_GRAY_DYE, ChatColor.GOLD + "Unlock Gray Sheep Pet", "100 Slime"));
-        }
-        
-        if (plugin.dataCosmetics.exists(player.getUniqueId(), "White Sheep Pet")) {
-            inv.setItem(29, createGuiItem(Material.WHITE_DYE, ChatColor.GOLD + "White Sheep Pet"));
-        }
-        else {
-            inv.setItem(29, createGuiItem(Material.WHITE_DYE, ChatColor.GOLD + "Unlock White Sheep Pet", "100 Slime"));
-        }
+        GuiConstructor(player, Material.RED_DYE, 10, 100, "Red Sheep Pet", "");
+        GuiConstructor(player, Material.ORANGE_DYE, 11, 100, "Orange Sheep Pet", "");
+        GuiConstructor(player, Material.YELLOW_DYE, 12, 100, "Yellow Sheep Pet", "");
+        GuiConstructor(player, Material.GREEN_DYE, 13, 100, "Green Sheep Pet", "");
+        GuiConstructor(player, Material.LIME_DYE, 14, 100, "Lime Sheep Pet", "");
+        GuiConstructor(player, Material.BLUE_DYE, 15, 100, "Blue Sheep Pet", "");
+        GuiConstructor(player, Material.CYAN_DYE, 16, 100, "Cyan Sheep Pet", "");
+        GuiConstructor(player, Material.LIGHT_BLUE_DYE, 19, 100, "Light Blue Sheep Pet", "");
+        GuiConstructor(player, Material.PURPLE_DYE, 20, 100, "Purple Sheep Pet", "");
+        GuiConstructor(player, Material.MAGENTA_DYE, 21, 100, "Magenta Sheep Pet", "");
+        GuiConstructor(player, Material.PINK_DYE, 22, 100, "Pink Sheep Pet", "");
+        GuiConstructor(player, Material.BROWN_DYE, 23, 100, "Brown Sheep Pet", "");
+        GuiConstructor(player, Material.BLACK_DYE, 24, 100, "Black Sheep Pet", "");
+        GuiConstructor(player, Material.GRAY_DYE, 25, 100, "Gray Sheep Pet", "");
+        GuiConstructor(player, Material.LIGHT_GRAY_DYE, 28, 100, "Light Gray Sheep Pet", "");
+        GuiConstructor(player, Material.WHITE_DYE, 29, 100, "White Sheep Pet", "");
 
         
         inv.setItem(39, createGuiItem(Material.ARROW, "Back"));
