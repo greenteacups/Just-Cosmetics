32ca96de68d1dd1ec689e6f008eaadea6e472f8d
KK964 <morrkade05@gmail.com>
2021-03-07 10:19:43 -0600

    stop disguises from working in gmsp

--- src/main/java/cosmetics/disguises/listeners/DisguiseGeneralListeners.java
+++ src/main/java/cosmetics/disguises/listeners/DisguiseGeneralListeners.java
@@ -2,6 +2,8 @@ package cosmetics.disguises.listeners;
 
 import java.util.HashMap;
 
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -15,10 +17,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.potion.PotionEffectType;
 
 import cosmetics.Cosmetics;
@@ -27,7 +26,10 @@ import cosmetics.RemoveEffectsOnQuit;
 
 public class DisguiseGeneralListeners implements Listener {
 
+    private final Cosmetics plugin;
+
     public DisguiseGeneralListeners(Cosmetics b) {
+        this.plugin = b;
     }
 
     public HashMap<Player, Entity> currentDisguise = DisguiseGuiListeners.currentDisguise;
@@ -139,7 +141,17 @@ public class DisguiseGeneralListeners implements Listener {
         Player player = event.getPlayer();
         RemoveEffectsOnQuit.ClearEffects(player);
     }
-    
 
+    @EventHandler
+    public void onGamemodeChange(PlayerGameModeChangeEvent e) {
+        Player player = e.getPlayer();
+        Entity disguise = currentDisguise.get(player);
+        if(disguise != null && e.getNewGameMode() == GameMode.SPECTATOR) {
+            disguise.remove();
+            currentDisguise.remove(player);
+            player.removePotionEffect(PotionEffectType.INVISIBILITY);
+            player.sendMessage(ChatColor.DARK_RED + "Your disguise was removed because you went into spectator mode!");
+        }
+    }
     
 }
